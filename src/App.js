2a0031dc92b95a06176a757fa1640cdeb4f3158f
jeo19 <terrakkk@outlook.com>
2018-12-13 00:00:29 +0630

    Add the SplitMe function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 class App extends Component {
+  state = {
+    SplitMe: null
+  };
   handleClick = () => {
-    import("./Notify").then(({ default: Notify }) => {
-      Notify();
+    import("./SplitMe").then(({ default: SplitMe }) => {
+      this.setState({
+        SplitMe
+      });
     });
   };
   render() {
+    const { SplitMe } = this.state;
     return (
       <div>
         <button onClick={this.handleClick}>Click me</button>
+        {SplitMe && <SplitMe />}
       </div>
     );
   }
