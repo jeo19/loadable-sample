708c403f5fed2257035f3b7c4fa94a9837a47052
jeo19 <terrakkk@outlook.com>
2018-12-18 01:58:59 +0630

    Updated the code with using react-loadable module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
-import withSplitting from "../withSplitting";
-export const Home = withSplitting(() => import("./Home"));
-export const About = withSplitting(() => import("./About"));
+import React from "react";
+import Loadable from "react-loadable";
+const Loading = () => {
+  return <div> Loading...</div>;
+};
+export const Home = () =>
+  Loadable({
+    loader: () => import("./Home"),
+    loading: Loading
+  });
+export const About = () =>
+  Loadable({
+    loader: () => import("./About"),
+    loading: Loading
+  });
